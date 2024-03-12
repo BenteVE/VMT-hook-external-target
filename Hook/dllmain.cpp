c502c2374f4335daa297253658c8acb9b1a48966
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-03-12 21:32:21 +0100

    Added console, refactored hook installation

--- Hook/dllmain.cpp
+++ Hook/dllmain.cpp
@@ -1,22 +1,9 @@
-/*
-    SOURCES:
-        - 
-
-    PROJECT SETTINGS:
-        - 
-
-    INFO:
-        - USAGE:
-            This code creates a DLL. When the DLL is injected in the companion application, 
-            it will install a hook on the Virtual Method Table. This will overwrite the
-            functions of that application with our own functions.
-
-        - 
-
-*/
 #include <iostream>
 #include <Windows.h>
 
+#include "Console.h"
+
+Console console;
 
 //we use this to store and call the original function inside our custom one
 // when defining a thiscall, we always have to give the pointer ourselves
@@ -32,71 +19,89 @@
 // we use __fastcall for our hook to so we can easily access the this pointer from the original __thiscall convention that the original function used
 // if we would use __thiscall for our hook instead, we wouldn't get access to the pointer as easily because the this pointer is not explicitly mentionned in the parameters
 void __fastcall hook(void* pEcx, void* pEdx) {
-    //Base* object = (Base*)pEcx; //argument in register ecx is a pointer to this object
-    std::cout << "This function was hooked" << std::endl;
+	//Base* object = (Base*)pEcx; //argument in register ecx is a pointer to this object
+	std::cout << "This function was hooked" << std::endl;
 
-    //call the original function
-    //g_org_VirtualFunction01(thisptr);
+	//call the original function
+	//g_org_VirtualFunction01(thisptr);
 }
 /*
 // werkt dit ook of kunnen we argument in Ecx niet bereiken met thiscall? (niet mogelijk om thiscall te definiëren buiten klasse?)
 void __thiscall hook(void* Ecx, void* arg1, ...) {
-    //Base* object = (Base*)pEcx; //argument in register ecx is a pointer to this object
-    std::cout << "This function was hooked" << std::endl;
+	//Base* object = (Base*)pEcx; //argument in register ecx is a pointer to this object
+	std::cout << "This function was hooked" << std::endl;
 
-    //call the original function
-    //g_org_VirtualFunction01(thisptr);
+	//call the original function
+	//g_org_VirtualFunction01(thisptr);
 }
 */
 
 
-DWORD WINAPI installVTableHook(PVOID base) {
+DWORD WINAPI installVTableHook() {
 
-    //find base address of module program 
-    DWORD imageBase = (DWORD)GetModuleHandle(L"Virtual Method Table.exe");
-    if (imageBase == NULL) {
-        MessageBox(NULL, L"Base not found", L"Base not found", MB_OK);
-        return FALSE;
-    }
-    
-    DWORD offsetFunction2 = 0x3274; //found in Ghidra (2nd function in VTable of Derived class!)
+	//find base address of module program 
+	UINT_PTR base = (UINT_PTR)GetModuleHandle(NULL);
+	if (base == NULL) {
+		fprintf(console.stream, "Module handle not found\n");
+		return FALSE;
+	}
 
-    PDWORD pFunction2 = (PDWORD)(imageBase + offsetFunction2);
-    std::cout << std::hex << imageBase << std::endl;
-    std::cout << std::hex << offsetFunction2 << std::endl;
-    std::cout << pFunction2 << std::endl;
+	// For the target program with x86 architecture,
+	// this is the offset of the second function in the Virtual Method Table of the Derived class
+	// This offset can be found by analyzing the program with for example Ghidra
+	UINT_PTR offset = 0x3274;
 
-    std::cout << std::hex << *pFunction2 << std::endl; //address of actual function in .exe
+	// A pointer to the address in the VMT that we will overwrite to install the hook
+	PUINT_PTR pHook = (PUINT_PTR)(base + offset);
 
-    // Get location of hook function, we will copy this location into the VTable
-    void* pHookFnc = (void*)hook;
+	// Check the original content of the VMT, this is the true address of function2() of the derived class
+	fprintf(console.stream, "Content of the VMT: %x", *pHook);
 
-    std::cout << std::hex << pHookFnc << std::endl; //address of our function on top of this file
+	// Install hook
+	DWORD oldProtect = 0;
+	VirtualProtect(pHook, sizeof(UINT_PTR), PAGE_EXECUTE_READWRITE, &oldProtect);
+	*pHook = (UINT_PTR)&hook;
+	VirtualProtect(pHook, sizeof(UINT_PTR), oldProtect, &oldProtect);
 
-    // Install hook
-    DWORD oldProtect = 0;
-    VirtualProtect(pFunction2, sizeof(PDWORD), PAGE_EXECUTE_READWRITE, &oldProtect);
-    memcpy(pFunction2, &pHookFnc, sizeof(PDWORD));
-    VirtualProtect(pFunction2, sizeof(PDWORD), oldProtect, &oldProtect);
+	// Check if the content is correctly overwritten
+	fprintf(console.stream, "Content of the VMT: %x", *pHook);
 
-    std::cout << std::hex << *pFunction2 << std::endl; //address in memory is changed to our address
-
-    MessageBox(NULL, L"Hook installed", L"Hook installed", MB_OK);
-
-    return TRUE;
+	return TRUE;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        CreateThread(nullptr, NULL, installVTableHook, hModule, NULL, nullptr); break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+
+	case DLL_PROCESS_ATTACH:
+	{
+		// The DisableThreadLibraryCalls function lets a DLL disable the DLL_THREAD_ATTACH and DLL_THREAD_DETACH notification calls.
+		// This can be a useful optimization for multithreaded applications that have many DLLs, frequently createand delete threads, 
+		// and whose DLLs do not need these thread - level notifications of attachment/detachment.
+		DisableThreadLibraryCalls(hModule);
+
+		if (!console.open()) {
+			// Indicate DLL loading failed
+			return FALSE;
+		}
+
+		// install hook
+		CreateThread(nullptr, NULL, LPTHREAD_START_ROUTINE(installVTableHook), NULL, NULL, nullptr);
+
+		return TRUE;
+	}
+	case DLL_THREAD_ATTACH: break;
+	case DLL_THREAD_DETACH: break;
+	case DLL_PROCESS_DETACH: {
+		fprintf(console.stream, "Uninstalling the hook ...\n");
+
+		// Open a MessageBox to allow reading the output
+		MessageBoxW(NULL, L"Press Ok to close", L"Closing", NULL);
+
+		return TRUE;
+	}
+	}
+	return TRUE;
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # VMT-hook-External
+
  An example of a Virtual Method Table hook on a simple application

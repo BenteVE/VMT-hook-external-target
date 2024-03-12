3f6352fa33cba45529cbb553fe26d76510b00f6f
BenteVE <44496102+BenteVE@users.noreply.github.com>
2024-03-12 21:02:15 +0100

    Updated the formatting for easier readability in the terminal

--- Target/Virtual Method Table.cpp
+++ Target/Virtual Method Table.cpp
@@ -12,28 +12,32 @@ int main()
 {
 	// Wait for an input to start using the functions 
 	// while waiting, we can inject the DLL that installs the Virtual Method Table (VMT) hook
+	std::cout << "Pausing the program, the hook can now be installed:" << std::endl;
 	system("pause");
 
+	std::cout << std::endl << "Creating the classes:" << std::endl;
+	Base* pBase = new Derived();
+	Derived derived;
+	
 	// Here we are calling the functions in the derived class with a pointer to the base class
 	// In this case, the VMT will be used for functions defined as virtual in the base class
 	// Note: in the Base class, only function2() is defined as virtual, 
 	// so only function2() will appear in the VMT, and we can only hook that function
-	std::cout << "Calling the functions with a pointer to the base class:" << std::endl;
-	Base* pBase = new Derived();
+	std::cout << std::endl << "Calling the functions with a pointer to the base class:" << std::endl;
 	pBase->function1();
 	pBase->function2();
 
-	Derived derived;
+	std::cout << std::endl;
+	
 	callFunctions(&derived);
 
 	// When calling function2() without a pointer to the base class, 
 	// the VMT will not be used, so the hook will not be executed
-	std::cout << "Calling the functions without a pointer to the base class:" << std::endl;
+	std::cout << std::endl << "Calling the functions directly with the derived class:" << std::endl;
 	derived.function1();
 	derived.function2();
 
-	// Wait to be able to read output before closing
-	system("pause");
+	std::cout << std::endl << "Destroying the classes:" << std::endl;
 
 	return 0;
 }
